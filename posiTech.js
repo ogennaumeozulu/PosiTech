5bf4bc188f10699bc41da1d0cf0d9e03f299bbea
ogennaumeozulu <ogennac@gmail.com>
2018-11-10 23:54:53 -0500

    hell

--- posiTech.js
+++ posiTech.js
@@ -1,12 +1,36 @@
-var i = 0;
-function clickyBoi(){
-  console.log(i);
-  i++
-}
+// var i = 0;
+// function clickyBoi(){
+//   console.log(i);
+//   i++
+// }
 
-//Stressed array
 
-var stressedQuotes = [all..for, always...come]
+//STRESSED
+var stressedQuotes = ['All the suffering, stress, and addiction comes from not realizing you already are what you are looking for. - Jon Kabat-Zinn',
+  'Always remember that your present situation is not your final destination. The best is yet to come. - Zig Zigler'];
+var stressedReflection = ['Is what is causing you to be stressed temporary?',
+  'Can you completely avoid the stressor?'];
+var stressedAction = ['Make a list of stressors and what you can do to get through them!',
+  'Talk to someone about your feelings!'];
+var stressedExercise = ['Do some yoga! It won’t hurt to try!',
+  'Meditate! Take some time to relax and be free from any worries!'];
+var stressedPositive = ['Yesterday is not ours to recover, but tomorrow is ours to win or lose. - Lyndon B. Johnson',
+  'Under any circumstance, simply do your best, and you will avoid self-judgment, self-abuse and regret. - Don Miguel Ruiz'];
+
+//LOST/DOUBTFUL
+var LostQuotes =['','']
+
+//CONFUSED/Indecisive
+
+
+
+//JOYFUL
+
+
+//TIRED
+
+
+
 
 if (randomEntry == 0){
   sdfkljsad print(stressedQuotes[0])
