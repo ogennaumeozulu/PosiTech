cf8326051e0ef06bba6982625c83d1f195ccbe10
Priyanka Kishore <prxyankakxshore@Priyankas-MacBook-Pro.local>
2018-11-11 01:10:02 -0500

    changes

--- posiTech.js
+++ posiTech.js
@@ -18,11 +18,28 @@ var stressedPositive = ['Yesterday is not ours to recover, but tomorrow is ours 
   'Under any circumstance, simply do your best, and you will avoid self-judgment, self-abuse and regret. - Don Miguel Ruiz'];
 
 //LOST/DOUBTFUL
-var LostQuotes =['','']
+var lostQuotes =['Sometimes the bad things that happen in our lives put us directly on the path to the best things that will ever happen to us. - Nicole Reed',
+  'Sometimes, you have to stop thinking so much and just go where your heart takes you. - Ben Francia'];
+var lostReflection = ['What do you feel is missing?',
+  'Where or when do you feel most comfortable or certain about yourself?'];
+var lostAction = ['Share your thoughts with someone you trust!',
+  'Set short-term goals that are easy to accomplish.'];
+var lotExercise = ['Meditate! Take some time to relax and be free from any worries.',
+  'Try something new! It’s okay to be outside of your comfort zone.'];
+var lostPositive = ['Everything is going to be okay in the end. If it’s not okay, then it means it’s not the end. - John Lennon',
+  'Say yes. You’ll figure it out afterward. - Tina Fey'];
 
 //CONFUSED/Indecisive
-
-
+var confusedQuotes = ['When you have to make a choice and don\'t make it, that is in itself a choice - William James',
+  'Make bold choices and make mistakes. It\'s all those things that add up to the person you become. - Angelina Jolie'];
+var confusedReflection = ['What is the source of your problem?',
+  'What do you need to think about in order to make a decision?'];
+var confusedAction = ['Make a pros and cons list!',
+  'Talk to someone you trust!'];
+var confusedExercise = ['Take a nap! You might wake up with a clearer head!',
+  'Go on a run! You might be able to use the time to think!'];
+var confusedPositive = ['Your positive action combined with positive thinking results in success. - Shiv Khera',
+  'Under any circumstance, simply do your best, and you will avoid self-judgment, self-abuse and regret. - Don Miguel Ruiz'];
 
 //JOYFUL
 
